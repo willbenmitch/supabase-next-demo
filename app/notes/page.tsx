7582f09c210946b1025f59d7c7d2ad1b3d5465e9
willbenmitch <ben.mitchell01@gmail.com>
2024-03-31 12:07:04 -0400

    init

---
+++ app/notes/page.tsx
@@ -0,0 +1,9 @@
+'use server';
+import { createClient } from '@/utils/supabase/server';
+
+export default async function Page() {
+  const supabase = createClient();
+  const { data: notes } = await supabase.from('notes').select();
+
+  return <pre>{JSON.stringify(notes, null, 2)}</pre>;
+}
